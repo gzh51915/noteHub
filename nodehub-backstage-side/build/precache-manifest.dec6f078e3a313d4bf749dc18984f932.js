d6227d02ebfc798a096b7249983d8b0b8770864f
谭着凉 <7374191+raininttt@user.noreply.gitee.com>
2020-06-22 16:25:52 +0800

    ok

---
+++ nodehub-backstage-side/build/precache-manifest.dec6f078e3a313d4bf749dc18984f932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6a73748ff9f79e7fad7805ff47b8c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d21e75454347cb49216",
+    "url": "/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "c352e5127a3aeaa033af",
+    "url": "/static/css/main.f702bf54.chunk.css"
+  },
+  {
+    "revision": "9d21e75454347cb49216",
+    "url": "/static/js/2.294875fc.chunk.js"
+  },
+  {
+    "revision": "8071c38011fb473a8a3ee1e1c449fc0f",
+    "url": "/static/js/2.294875fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c352e5127a3aeaa033af",
+    "url": "/static/js/main.bd51350a.chunk.js"
+  },
+  {
+    "revision": "b14175ae0fcf343723ac",
+    "url": "/static/js/runtime-main.aadef3c0.js"
+  },
+  {
+    "revision": "63d170101781559092e49c102dea13eb",
+    "url": "/static/media/logo.63d17010.png"
+  }
+]);
